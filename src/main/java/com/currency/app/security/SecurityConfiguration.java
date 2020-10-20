63ba24b2d603a62a9e3cb8a3a269ebc59137f777
madamczyk <michal.adamczyk@bluesoft.com>
2020-10-20 17:52:00 +0200

    Dodanie swaggera

--- src/main/java/com/currency/app/security/SecurityConfiguration.java
+++ src/main/java/com/currency/app/security/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.currency.app.security;
 
 import com.currency.app.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -10,6 +11,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
@@ -18,10 +21,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     private final UserService userDetailsService;
 
+    @Value("${server.servlet.context-path}")
+    private static String apiPrefix;
+
     @Bean
     public PasswordEncoder encoder() {
         return new Pbkdf2PasswordEncoder("53cr3t");
     }
+
+    private static final String[] PERMIT_ALL_URLS = {
+            apiPrefix +  "/user/registration"
+    };
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -31,11 +41,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-            .antMatchers("/api/v1/account", "/api/v1/exchange")
+            .antMatchers(apiPrefix + "/account", apiPrefix + "/exchange")
             .access("hasRole('ROLE_USER')")
-            .antMatchers("/api/v1/user/registration")//"/api/v1/h2-console/**")
+            .antMatchers(PERMIT_ALL_URLS)//"/api/v1/h2-console/**")
             .permitAll();
         http.csrf().disable();
-        //http.headers().frameOptions().disable();
     }
 }
